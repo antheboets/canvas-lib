b9d50157e2678c2201dfab55f472b2f12c2b6a38
antheboets <anthe.boets@student.ehb.be>
2022-10-09 18:29:59 +0200

    adding total time in fps menu and beter formating in fps menu

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 export function getFpsString(currentFps = 0,getMaxFps = 0){
     let spacing = ""
     for(let i =0; i + currentFps.toString().length < getMaxFps.toString().length;i++){
-        spacing += "a"
+        spacing += " "
     }
     return `${spacing}${currentFps}/${getMaxFps}fps`
 }
