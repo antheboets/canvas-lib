6a9458ba076dfe7e0c09a4d052d45fab16ae2e88
antheboets <anthe.boets@student.ehb.be>
2022-10-09 18:16:07 +0200

    adding function for beter fps printing

--- helper.js
+++ helper.js
@@ -1,1 +1,7 @@
-//function 
+export function getFpsString(currentFps = 0,getMaxFps = 0){
+    let spacing = ""
+    for(let i =0; i + currentFps.toString().length < getMaxFps.toString().length;i++){
+        spacing += "a"
+    }
+    return `${spacing}${currentFps}/${getMaxFps}fps`
+}
