a0e18ea9c7141287a31ba235050ddf07e0cc3eba
antheboets <anthe.boets@student.ehb.be>
2022-10-13 17:33:56 +0200

    adding helper function to get the file exstension from path

--- helper.js
+++ helper.js
@@ -1,1 +1,12 @@
-//function 
+export function getTypeOfFileFromPath(path){
+    switch(path.split('.').pop().toLowerCase()){
+        case 'mp4':
+            return 'video'
+        case 'mp3':
+            return 'audio'
+        case 'png':
+            return 'image'
+        default:
+            return 'none'
+    }
+}
