849d2a6c3bc3441719698b539e4828485a81764c
antheboets <anthe.boets@student.ehb.be>
2022-10-17 01:40:30 +0200

    fixing bug

--- helper.js
+++ helper.js
@@ -2,7 +2,7 @@ export function getTypeOfFileFromPath(path=""){
     if(path === undefined || path === null){
         return 'none'
     }
-    if(typeof path === 'string'){
+    if(typeof path !== 'string'){
         return 'none'
     }
     switch(path.split('.').pop().toLowerCase()){
