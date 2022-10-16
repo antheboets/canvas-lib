4f33c53fe5719a97b3d1f6eed944765e109730e6
antheboets <anthe.boets@student.ehb.be>
2022-10-16 20:28:42 +0200

    adding factory file

---
+++ Factory.js
@@ -0,0 +1,15 @@
+//config/template
+export function ContentFactory(){
+    return {
+        contentType:"",
+        path:"",
+        time:0
+    }
+}
+//config/template
+export function LayerFactory(){
+    return {
+        mode:"",
+        conten:[]
+    }
+}
