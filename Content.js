600d8ebfc2e34fa447c07efe297c1fc1197fd260
antheboets <anthe.boets@student.ehb.be>
2022-10-31 17:35:21 +0100

    removing unused code

--- Content.js
+++ Content.js
@@ -6,20 +6,6 @@ import Size from './Size.js'
 const DefaultTimeoutTime = (2 * 1000) + 978
 
 export class Content{
-    /*
-    #height
-    #width
-    #widthMode
-    #heightMode
-    #nativeHeight
-    #nativeWidth
-    */
-    /*
-    #xPos
-    #xPercent
-    #yPos
-    #yPercent
-    */
     constructor(obj,loadedPromise,options){
         this.contentObj = obj
         this.loadedPromise = loadedPromise
@@ -29,22 +15,6 @@ export class Content{
         this.y = new AxisCoordinate(options.y,()=>{return getCanvas().canvasElement.height})
         this.height = new Size(options.height,options.heightMode,()=>{return getCanvas().canvasElement.height})
         this.width = new Size(options.width,options.widthMode,()=>{return getCanvas().canvasElement.width})
-        /*
-        this.setXPercent = false
-        this.setYPercent = false
-        this.setXPos = options.x
-        this.setYPos = options.y
-        */
-        /*
-        this.useCanvasHeight = false
-        this.useCanvasWidth = false
-        this.setNativeWidth = 0
-        this.setMativeHeight = 0
-        this.setHeightMode = options.heightMode
-        this.setWidthMode = options.widthMode
-        this.setHeight = options.height
-        this.setWidth = options.width
-        */
     }
     GetTimeoutTime(){
         if(this.timeoutFunc !== null){
