a5e4803e3ce1688bbd83dd5098b1d4f63bffd797
antheboets <anthe.boets@student.ehb.be>
2022-10-31 17:41:39 +0100

    removing code that is moved to Size class

--- Content.js
+++ Content.js
@@ -30,176 +30,35 @@ export class Content{
     }
     draw(ctx,canvasWidth = 0,canvasHeight = 0){}
     set setHeight(height){
-        /*
-        let factor = 1
-        if(this.#isPercent(height)){
-            factor = convertPercent(height)
-        }
-        switch(this.getHeightMode){
-            case "manual":
-                if(this.#isPercent(height)){
-                    this.#height = Math.round(factor * this.#nativeHeight)
-                }
-                else{
-                    this.#height = height
-                }
-                break
-            case "canvasSize":
-                this.#height = getCanvas().canvasElement.height
-                break
-            case "native":
-                this.#height = Math.round(factor * this.#nativeHeight)
-                break
-            default:
-                break
-        }
-        */
        this.height.setSize = height
     }
     get getHeight(){
         return this.height.getSize
     }
     set setWidth(width){
-        /*
-        let factor = 1
-        if(this.#isPercent(width)){
-            factor = convertPercent(width)
-        }
-        switch(this.getWidthMode){
-            case "manual":
-                if(this.#isPercent(width)){
-                    this.#width = Math.round(factor * this.#nativeWidth)
-                }
-                else{
-                    this.#width = width
-                }
-                break
-            case "canvasSize":
-                this.#width = getCanvas().canvasElement.width
-                break
-            case "native":
-                this.#width = Math.round(factor * this.#nativeWidth)
-                break
-            default:
-                break
-        }
-        */
        this.width.setSize = width
     }
     get getWidth(){
         return this.width.getSize
     }
     set setHeightMode(heightMode){
-        /*
-        switch(heightMode){
-            case "manual":
-                this.#heightMode = "manual"
-                break
-            case "canvasSize":
-                this.useCanvasHeight = true
-                this.#heightMode = "canvasSize"
-                this.#height = getCanvas().canvasElement.height
-                break
-            case "native":
-                this.#heightMode = "native"
-                this.#height = this.#nativeHeight
-                break
-            default:
-                this.#heightMode = "manual"
-                break
-        }
-        if(this.useCanvasWidth && this.#heightMode !== "canvasSize"){
-            this.useCanvasHeight = false
-        }
-        */
         this.height.setMode = heightMode
     }
     get getHeightMode(){
         return this.height.getMode
     }
     set setWidthMode(widthMode){
-        /*
-        switch(widthMode){
-            case "manual":
-                this.#widthMode = "manual"
-                break
-            case "canvasSize":
-                this.useCanvasWidth = true
-                this.#widthMode = "canvasSize"
-                this.#width = getCanvas().canvasElement.width
-                break
-            case "native":
-                this.#widthMode = "native"
-                this.#width = this.#nativeWidth
-                break
-            default:
-                this.#widthMode = "manual"
-                break
-        }
-        if(this.useCanvasWidth && this.#widthMode !== "canvasSize"){
-            this.useCanvasWidth = false
-        }
-        */
-        //this.#widthMode = this.#sizeMode(widthMode)
         this.width.setMode = widthMode
     }
     get getWidthMode(){
         return this.width.getMode
     }
-    /*
-    #sizeMode(mode = ""){
-        switch(mode){
-            case "manual":
-                return "manual"
-            case "canvasSize":
-                return "canvasSize"
-            case "native":
-                return "native"
-            default:
-                return "manual"
-        }
-    }
-    */
     get getNativeHeight(){
         return this.height.getNativeSize
     }
-    /*
-    set setNativeHeight(nativeHeight){
-        if(this.#heightMode === "native"){
-            this.#height = nativeHeight
-        }
-        this.#nativeHeight = nativeHeight
-    }
-    */
     get getNativeWidth(){
         return this.width.getNativeSize
     }
-    /*
-    set setNativeWidth(nativeWidth){
-        if(this.#widthMode === "native"){
-            this.#width = nativeWidth
-        }
-        this.#nativeWidth = nativeWidth
-    }
-    */
-   /*
-    #isPercent(value){
-        if(value === undefined || value === null){
-            return false
-        }
-        if(typeof value !== 'string'){
-            return false
-        }
-        if(value.slice(-1) !== '%'){
-            return false
-        }
-        let number = Number(value.slice(0,value.length -1))
-        if(number === NaN){
-            return false
-        }
-        return true
-    }
-    */
     set setXPos(xPos){
         this.x.setCoordinate(xPos)
         /*
