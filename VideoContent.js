92a0f0158efd01c3188e4e2539e03712b17df641
antheboets <anthe.boets@student.ehb.be>
2022-11-01 17:31:10 +0100

    updating video class

--- VideoContent.js
+++ VideoContent.js
@@ -1,11 +1,28 @@
 import Content from "./Content.js"
+import {getCanvas} from './CanvasSingleton.js'
+
 
 export class VideoContainer extends Content{
-    constructor(url,loadedPromise){
-        super(url,loadedPromise)
+    constructor(options){
+        const object = document.createElement('video')
+        object.muted = true
+        object.autoplay = true
+        object.loop = true
+        const loadPromise = new Promise((resolve, reject)=>{
+            object.oncanplay = ()=>{
+                this.height.setNativeSize = object.height
+                this.width.setNativeSize = object.width
+                resolve()
+            }
+        })
+        object.src = options.path
+        const videoContainer = {video:this.object,ready:false,scale:Math.min(getCanvas().canvasElement.width / this.getWidth,getCanvas().canvasElement.height / this.getHeight)}
+        super(videoContainer,loadPromise,options)
     }
-    draw(ctx,canvasWidth = 0,canvasHeight = 0){
-        ctx.drawImage(this.contentObj,0,0,canvasWidth,canvasHeight)
+    draw(ctx){
+        if(this.videoContainer !== undefined && this.videoContainer.ready){
+            ctx.drawImage(this.contentObj.video,this.getXPos,this.getYPos,this.getWidth,this.getHeight)
+        }
     }
 }
 export default VideoContainer
