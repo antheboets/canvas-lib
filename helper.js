d5e0f14212910738f59cbcdc65358a0c2b4bcc9d
antheboets <anthe.boets@student.ehb.be>
2022-11-01 18:30:52 +0100

    adding webm to getTypeOfFileFromPath

--- helper.js
+++ helper.js
@@ -8,6 +8,8 @@ export function getTypeOfFileFromPath(path=""){
     switch(path.split('.').pop().toLowerCase()){
         case 'mp4':
             return 'video'
+        case 'webm':
+            return 'video'
         case 'mp3':
             return 'audio'
         case 'png':
