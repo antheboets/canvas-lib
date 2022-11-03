aee962d6e06d78e5b39ef6b97c02a39d2b26e476
antheboets <anthe.boets@student.ehb.be>
2022-11-03 18:34:11 +0100

    updating ImageContent

--- ImageContent.js
+++ ImageContent.js
@@ -1,11 +1,21 @@
-import Content from './Content.js'
+import Loaded2dContent from './Loaded2dContent.js'
 
-export class ImageContent extends Content{
-    constructor(url,loadedPromise,obj){
-        super(url,loadedPromise,obj)  
+export class ImageContent extends Loaded2dContent{
+    constructor(obj){
+        const image = new Image()
+        super(obj,(resolve, reject)=>{
+            image.onload = ()=>{
+                //add native size
+                this.width.setNativeSize = image.width
+                this.height.setNativeSize = image.height
+                resolve()
+            }
+        })
+        this.image = image
+        this.image.src = obj.path
     }
     draw(ctx){
-        ctx.drawImage(this.contentObj,this.getXPos,this.getYPos,this.getWidth,this.getHeight)
+        ctx.drawImage(this.image,this.getXPos,this.getYPos,this.getWidth,this.getHeight)
     }
 }
 export default ImageContent
