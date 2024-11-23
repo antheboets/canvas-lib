29cf9eefaab09c9951f088a00c2028065be50e30
anthe <anthe@debian.home>
2024-11-23 17:04:27 +0100

    adding gameobject class

---
+++ GameObject.js
@@ -0,0 +1,11 @@
+
+export class GameObject{
+    constructor(x,y,hitbox,content){
+        this.x = x
+        this.y = y
+        this.hitbox = hitbox
+        this.content = content
+    }
+}
+
+export default GameObject
