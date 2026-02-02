1d2498d88772591a7c1b8a433bb14678ec26c52b
anthe <anthe@debian.home>
2026-02-02 23:47:38 +0100

    adding some more params for the text render

--- TextContent.js
+++ TextContent.js
@@ -1,23 +1,32 @@
 import TwoDContent from "./TwoDContent";
 
 export class TextContent extends TwoDContent{
-    constructor({text="",font="",size="12px",color="black"}){
+    constructor({text="",font="",size="12px",color="black",textBaseline="alphabetic",textAlign="start",direction="inherit"}){
         super({...arguments[0]})
         this.text = text
         this.font = font
         this.size = size
         this.color = color
+        this.textAlign = textAlign
+        this.textBaseline = textBaseline
+        this.direction = direction
     }
     draw(ctx){
         ctx.font = this.size + " " + this.font
         ctx.strokeStyle = this.color
         ctx.fillStyle = this.color
+        ctx.direction = this.direction
+        ctx.textAlign = this.textAlign
+        ctx.textBaseline = this.textBaseline
         ctx.fillText(this.text,this.getXPos,this.getYPos)
     }
     drawMulti(ctx,x,y){
         ctx.font = this.size + " " + this.font
         ctx.strokeStyle = this.color
         ctx.fillStyle = this.color
+        ctx.direction = this.direction
+        ctx.textAlign = this.textAlign
+        ctx.textBaseline = this.textBaseline
         ctx.fillText(this.text, x + this.getXPos,y + this.getYPos)
     }
 }
